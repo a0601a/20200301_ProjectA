07b3ca15d90aecbd0da6e5fd6c958fd48ac4a0ac
Jacky <jacky8595125@gmail.com>
2020-04-27 18:03:13 +0800

    fix banner

--- assets/javaScript/banner.js
+++ assets/javaScript/banner.js
@@ -40,80 +40,84 @@
 
 
 // == //
-$(function(){
-  var element = $('.top100Of')
-  var tl = getGSAPSetting({
-    font: element.find('.font')
-  })
-  element.mouseenter(function(e){
-    var self = e.target
-    tl.play()
-  })
-  element.mouseover(function(e){
-    var self = e.target
-    tl.pause()
-    tl.restart()
-  })
-});
-$(function(){
-  var element = $('.about')
-  var tl = getGSAPSetting({
-    font: element.find('.font')
-  })
-  element.mouseenter(function(e){
-    var self = e.target
-    tl.play()
-  })
-  element.mouseover(function(e){
-    var self = e.target
-    tl.pause()
-    tl.restart()
-  })
-});
-$(function(){
-  var element = $('.howToUse')
-  var tl = getGSAPSetting({
-    font: element.find('.font')
-  })
-  element.mouseenter(function(e){
-    var self = e.target
-    tl.play()
-  })
-  element.mouseover(function(e){
-    var self = e.target
-    tl.pause()
-    tl.restart()
-  })
-});
-$(function(){
-  var element = $('.staff')
-  var tl = getGSAPSetting({
-    font: element.find('.font')
-  })
-  element.mouseenter(function(e){
-    var self = e.target
-    tl.play()
-  })
-  element.mouseover(function(e){
-    var self = e.target
-    tl.pause()
-    tl.restart()
-  })
-});
+if( false ){
+
+
+  $(function(){
+    var element = $('.top100Of')
+    var tl = getGSAPSetting({
+      font: element.find('.font')
+    })
+    element.mouseenter(function(e){
+      var self = e.target
+      tl.play()
+    })
+    element.mouseover(function(e){
+      var self = e.target
+      tl.pause()
+      tl.restart()
+    })
+  });
+  $(function(){
+    var element = $('.about')
+    var tl = getGSAPSetting({
+      font: element.find('.font')
+    })
+    element.mouseenter(function(e){
+      var self = e.target
+      tl.play()
+    })
+    element.mouseover(function(e){
+      var self = e.target
+      tl.pause()
+      tl.restart()
+    })
+  });
+  $(function(){
+    var element = $('.howToUse')
+    var tl = getGSAPSetting({
+      font: element.find('.font')
+    })
+    element.mouseenter(function(e){
+      var self = e.target
+      tl.play()
+    })
+    element.mouseover(function(e){
+      var self = e.target
+      tl.pause()
+      tl.restart()
+    })
+  });
+  $(function(){
+    var element = $('.staff')
+    var tl = getGSAPSetting({
+      font: element.find('.font')
+    })
+    element.mouseenter(function(e){
+      var self = e.target
+      tl.play()
+    })
+    element.mouseover(function(e){
+      var self = e.target
+      tl.pause()
+      tl.restart()
+    })
+  });
 
 
 
-function getGSAPSetting (obj) {
-  var tl = new TimelineLite({paused: true})
-  
-  // gsap 動畫
-  var mySplitText = new SplitText(obj.font, {type:"chars, words"})
-  var chars = mySplitText.chars;
-  // gsap.set(obj.font, {perspective: 400});
-  tl.from(chars, {autoAlpha:0, visibility:"hidden"})
-    .staggerFrom(chars, 0.7, {duration: 2.8, y:100, transformOrigin:"50% 50%"}, 0.02)
-    .to(chars, 0.7, { autoAlpha:0, visibility:"hidden" })
+  function getGSAPSetting (obj) {
+    var tl = new TimelineLite({paused: true})
+    
+    // gsap 動畫
+    var mySplitText = new SplitText(obj.font, {type:"chars, words"})
+    var chars = mySplitText.chars;
+    // gsap.set(obj.font, {perspective: 400});
+    tl.from(chars, {autoAlpha:0, visibility:"hidden"})
+      .staggerFrom(chars, 0.7, {duration: 2.8, y:100, transformOrigin:"50% 50%"}, 0.02)
+      .to(chars, 0.7, { autoAlpha:0, visibility:"hidden" })
 
 
-  return tl 
+    return tl 
+  }
 }
