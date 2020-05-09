84e426909d9d4342d1aa577c8283d9031777a67f
Jacky <jacky8595125@gmail.com>
2020-05-09 16:22:41 +0800

    ....

--- assets/javaScript/facebookShare.js
+++ assets/javaScript/facebookShare.js
@@ -1,15 +1,25 @@
-
-
 function fbShareTemplate(obj) {
   return (
-    '<div id="asdfasdf" class="fb-share-button" data-href="{{data-href}}" data-layout="button" data-size="small">' +
+    '<div id="fbShareBtn" class="fb-share-button" data-href="{{data-href}}" data-layout="button" data-size="small">' +
     '   <a target="_blank" href="{{href}}" class="fb-xfbml-parse-ignore">分享</a>' +
     '</div>'
-  ).replace(/{{data-href}}/g, obj.dataHref)
-  .replace(/{{href}}/g, obj.href)
+  )
+    .replace(/{{data-href}}/g, obj.dataHref)
+    .replace(/{{href}}/g, obj.href)
 }
 
 function fbShare(obj) {
   var fbDom = $('#fb')
-  if ( fbDom ) fbDom.append(fbShareTemplate(obj))
+  if (fbDom) fbDom.append(fbShareTemplate(obj))
 }
+
+$(function () {
+  $(document).on('click', '#facebookBtn', function (e) {
+    var self = e.target
+    var fbShareBtnDom = $('#fbShareBtn')
+    // if (self) if (fbShareBtnDom) fbShareBtnDom.click()
+
+
+
+  })
+})
